02c4d6511cc37af7d69f0da6f077a009924a2009
akritkbehera <akrit.kumar.behera@cern.ch>
2025-12-17 17:20:26 +0100

    Proper json dependencies

--- bits_helpers/spec_template.sh
+++ bits_helpers/spec_template.sh
@@ -34,4 +34,22 @@ if [ "$PKGNAME" != defaults-* ] && [ -f "$WORK_DIR/SPECS/$ARCHITECTURE/$PKGNAME/
     --define "_topdir $WORK_DIR/rpmbuild" \
     --define "buildroot $WORK_DIR/rpmbuild/BUILDROOT/${PKGNAME}" \
     "$WORK_DIR/rpmbuild/SPECS/${PKGNAME}.spec"
+  
+  for f in "${requires[@]}"; do
+    rpm_file="${f}-1-1.$(uname -m).rpm"
+    rpm -qp --queryformat "%{NAME}\n[%{PROVIDES}\n]" "$WORK_DIR/rpmbuild/RPMS/$(uname -m)/$rpm_file" | \
+    jq -R -s '
+      split("\n")
+      | map(select(length > 0))
+      | { (.[0]): { "provides": .[1:] } }   
+    '
+  done | jq -s 'add' > "$WORK_DIR/$ARCHITECTURE/$PKGNAME/$PKGVERSION-$PKGREVISION/provides.json"
+
+  rpm -qp --queryformat "%{NAME}\n[%{REQUIRES}\n]" "$WORK_DIR/rpmbuild/RPMS/$(uname -m)/${PKGNAME}_${PKGVERSION}_${PKGREVISION}_${PKGHASH}-1-1.$(uname -m).rpm" | \
+  jq -R -s '
+    split("\n")
+    | map(select(length > 0))
+    | { (.[0]): { "requires": .[1:] } }
+  ' > "$WORK_DIR/$ARCHITECTURE/$PKGNAME/$PKGVERSION-$PKGREVISION/requires.json"
+
 fi
